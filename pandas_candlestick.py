f5030c0d36a7773b0b8ebfb633bb81af5cddabf0
Taeke <taeke.dehaan@live.nl>
2018-09-30 23:02:57 +0200

    Switched to another API...

--- pandas_candlestick.py
+++ pandas_candlestick.py
@@ -12,7 +12,7 @@ from mpl_finance import candlestick_ohlc
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def pandas_candlestick_ohlc(dat, stick = "day", adj = False, otherseries = None):
+def pandas_candlestick_ohlc(dat, stick = "day", adj = False, otherseries = None, fields = ["Open", "High", "Low", "Close"]):
     
     """
     :param dat: pandas DataFrame object with datetime64 index, and float columns "Open", "High", "Low", and "Close", likely created via DataReader from "yahoo"
@@ -27,11 +27,10 @@ def pandas_candlestick_ohlc(dat, stick = "day", adj = False, otherseries = None)
     dayFormatter = DateFormatter('%d')      # e.g., 12
  
     # Create a new DataFrame which includes OHLC data for each period specified by stick input
-    fields = ["Open", "High", "Low", "Close"]
     if adj:
         fields = ["Adj. " + s for s in fields]
     transdat = dat.loc[:,fields]
-    transdat.columns = pd.Index(["Open", "High", "Low", "Close"])
+    transdat.columns = pd.Index([fields[0], fields[1], fields[2], fields[3]])
     if (type(stick) == str):
         if stick == "day":
             plotdat = transdat
@@ -43,12 +42,12 @@ def pandas_candlestick_ohlc(dat, stick = "day", adj = False, otherseries = None)
                 transdat["month"] = pd.to_datetime(transdat.index).map(lambda x: x.month) # Identify months
             transdat["year"] = pd.to_datetime(transdat.index).map(lambda x: x.isocalendar()[0]) # Identify years
             grouped = transdat.groupby(list(set(["year",stick]))) # Group by year and other appropriate variable
-            plotdat = pd.DataFrame({"Open": [], "High": [], "Low": [], "Close": []}) # Create empty data frame containing what will be plotted
+            plotdat = pd.DataFrame({fields[0]: [], fields[1]: [], fields[2]: [], fields[3]: []}) # Create empty data frame containing what will be plotted
             for name, group in grouped:
-                plotdat = plotdat.append(pd.DataFrame({"Open": group.iloc[0,0],
-                                            "High": max(group.High),
-                                            "Low": min(group.Low),
-                                            "Close": group.iloc[-1,3]},
+                plotdat = plotdat.append(pd.DataFrame({fields[0]: group.iloc[0,0],
+                                            fields[1]: max(group.High),
+                                            fields[2]: min(group.Low),
+                                            fields[3]: group.iloc[-1,3]},
                                            index = [group.index[0]]))
             if stick == "week": stick = 5
             elif stick == "month": stick = 30
@@ -57,12 +56,12 @@ def pandas_candlestick_ohlc(dat, stick = "day", adj = False, otherseries = None)
     elif (type(stick) == int and stick >= 1):
         transdat["stick"] = [np.floor(i / stick) for i in range(len(transdat.index))]
         grouped = transdat.groupby("stick")
-        plotdat = pd.DataFrame({"Open": [], "High": [], "Low": [], "Close": []}) # Create empty data frame containing what will be plotted
+        plotdat = pd.DataFrame({fields[0]: [], fields[1]: [], fields[2]: [], fields[3]: []}) # Create empty data frame containing what will be plotted
         for name, group in grouped:
-            plotdat = plotdat.append(pd.DataFrame({"Open": group.iloc[0,0],
-                                        "High": max(group.High),
-                                        "Low": min(group.Low),
-                                        "Close": group.iloc[-1,3]},
+            plotdat = plotdat.append(pd.DataFrame({fields[0]: group.iloc[0,0],
+                                        fields[1]: max(group.High),
+                                        fields[2]: min(group.Low),
+                                        fields[3]: group.iloc[-1,3]},
                                        index = [group.index[0]]))
  
     else:
@@ -83,8 +82,8 @@ def pandas_candlestick_ohlc(dat, stick = "day", adj = False, otherseries = None)
     ax.grid(True)
  
     # Create the candelstick chart
-    candlestick_ohlc(ax, list(zip(list(date2num(plotdat.index.tolist())), plotdat["Open"].tolist(), plotdat["High"].tolist(),
-                      plotdat["Low"].tolist(), plotdat["Close"].tolist())),
+    candlestick_ohlc(ax, list(zip(list(date2num(plotdat.index.tolist())), plotdat[fields[0]].tolist(), plotdat[fields[1]].tolist(),
+                      plotdat[fields[2]].tolist(), plotdat[fields[3]].tolist())),
                       colorup = "black", colordown = "red", width = stick * .4)
  
     # Plot other series (such as moving averages) as lines
--- quandl_test.py
+++ quandl_test.py
@@ -10,7 +10,7 @@ import quandl
 import datetime
 import pandas_candlestick as candlestick
 import numpy as np
-import pandas_datareader.data as web    # Going to get SPY from Yahoo!
+#import pandas_datareader.data as web    # Going to get SPY from Yahoo!
 
 
 # We will look at stock prices over the past year, starting at January 1, 2016
@@ -23,9 +23,9 @@ stockCodes = ["AAPL", "MSFT", "GOOG"]
 apple, microsoft, google = (quandl.get("WIKI/" + stockCode, start_date=start, end_date=end, api_key = apiKey) for stockCode in stockCodes)
  
 # Create a DataFrame consisting of the adjusted closing price of these stocks, first by making a list of these objects and using the join method
-stocks = pd.DataFrame({"AAPL": apple["Adj. Close"],
-                      "MSFT": microsoft["Adj. Close"],
-                      "GOOG": google["Adj. Close"]})
+stocks = pd.DataFrame({"AAPL": apple["Close"],
+                      "MSFT": microsoft["Close"],
+                      "GOOG": google["Close"]})
 
 
 stocks.head() # display result
@@ -34,7 +34,7 @@ stocks.plot(grid = True)
 # pylab.rcParams['figure.figsize'] = (15, 9)   # Change the size of plots
  
 # apple["Adj. Close"].plot(grid = True) # Plot the adjusted closing price of AAPL
-candlestick.pandas_candlestick_ohlc(apple, adj=True, stick="month")
+candlestick.pandas_candlestick_ohlc(apple, adj=False, stick="month")
 
 # df.apply(arg) will apply the function arg to each column in df, and return a DataFrame with the result
 # Recall that lambda x is an anonymous function accepting parameter x; in this case, x will be a pandas Series object
@@ -47,19 +47,4 @@ stock_change = stocks.apply(lambda x: np.log(x) - np.log(x.shift(1))) # shift mo
 
 stock_return.plot(grid = True).axhline(y = 1, color = "black", lw = 2)
 stock_change.plot(grid = True).axhline(y = 1, color = "black", lw = 2)
-
-
-spyder = web.DataReader("SPY", "yahoo", start, end)    # Didn't work
-# spyder = web.DataReader("SPY", "google", start, end)    # Didn't work either
-# If all else fails, read from a file, obtained from here: http://www.nasdaq.com/symbol/spy/historical
-# spyderdat = pd.read_csv("/home/curtis/Downloads/HistoricalQuotes.csv")    # Obviously specific to my system; set to
-                                                                          # location on your machine
-spyderdat = pd.DataFrame(spyderdat.loc[:, ["open", "high", "low", "close", "close"]].iloc[1:].as_matrix(),
-                         index=pd.DatetimeIndex(spyderdat.iloc[1:, 0]),
-                         columns=["Open", "High", "Low", "Close", "Adj Close"]).sort_index()
- 
-spyder = spyderdat.loc[start:end]
- 
-stocks = stocks.join(spyder.loc[:, "Adj Close"]).rename(columns={"Adj Close": "SPY"})
-stocks.head()
 
